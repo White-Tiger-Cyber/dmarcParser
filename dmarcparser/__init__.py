a7ea0590e95b749ca14e38ddb97a390dd044c411
White-Tiger-Cyber <tiger@whitetigercyber.com>
2025-08-19 17:55:26 -0400

    feat: initial public release v0.1.0

--- dmarcparser/__init__.py
+++ dmarcparser/__init__.py
@@ -1,1 +1,11 @@
+# dmarcparser/__init__.py
 __version__ = "0.1.0"
+
+# pyproject.toml (ensure these)
+[project]
+name = "dmarcparser"
+version = "0.1.0"
+requires-python = ">=3.9"
+# ...
+[project.scripts]
+dP = "dmarcparser.cli:main"
